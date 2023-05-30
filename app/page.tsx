2dfcd5d14add424977e7562feefda49b703d72b0
HOSU HAN <tnghgks@naver.com>
2023-05-31 00:21:26 +0900

    feat: Image Slider 컴포넌트 생성 (#1)
    * chore: react-icons install
    
    * feat: ImageSlider 컴포넌트 생성
    
    * design: select-none 옵션 추가
    
    * fix: Header 컴포넌트 z-index 추가
    
    * design: 버튼 크기 확대 및 슬라이드 버튼 중앙정렬

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,13 @@ export default function Home() {
       <Header />
       <main>
         <section>
-          <ImageSlider />
+          <ImageSlider
+            images={[
+              "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+              "https://jangmidang.s3.ap-northeast-2.amazonaws.com/d39ce942635a43f8b471e6ddebb1dae2.jpg",
+              "https://jangmidang.s3.ap-northeast-2.amazonaws.com/bd1c1f13194d4127b1a808851d5dc1cc_1440.jpg",
+            ]}
+          />
           <div className=" w-full h-[301px] gap-5 flex justify-start px-11 mt-9 mb-6">
             <IntroCard bgSrc="https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg">
               아이디어스
--- components/common/ImageSlider.tsx
+++ components/common/ImageSlider.tsx
@@ -1,3 +1,67 @@
-export default function ImageSlider() {
-  return <div className="w-100% h-[550px] bg-slate-600">ImageSlider</div>;
+"use client";
+
+import { useEffect, useRef, useState, useCallback } from "react";
+import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
+interface IProps {
+  images: string[];
+}
+
+export default function ImageSlider({ images = [] }: IProps) {
+  const [current, setCurrent] = useState(0);
+  const timerId = useRef<NodeJS.Timer>();
+
+  const prevSlide = useCallback(() => {
+    const isFirst = current === 0;
+    const newIndex = isFirst ? images.length - 1 : current - 1;
+    setCurrent(newIndex);
+  }, [current, images]);
+
+  const nextSlide = useCallback(() => {
+    const isLast = current === images.length - 1;
+    const newIndex = isLast ? 0 : current + 1;
+    setCurrent(newIndex);
+  }, [current, images]);
+
+  const moveSlide = (newIndex: number) => {
+    setCurrent(newIndex);
+  };
+
+  useEffect(() => {
+    timerId.current = setInterval(() => {
+      nextSlide();
+    }, 3000);
+
+    return () => clearInterval(timerId.current);
+  }, [nextSlide]);
+
+  return (
+    <div className="w-full h-[550px] ">
+      <div className="w-full h-full overflow-hidden flex relative group duration-500">
+        <img
+          src={images[current]}
+          alt={images[current]}
+          className={`w-full h-full object-cover shrink-0 select-none select-none`}
+        />
+        <BsChevronCompactLeft
+          onClick={prevSlide}
+          className="hidden group-hover:block absolute top-[50%] translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer select-none w-10 h-10"
+        />
+        <BsChevronCompactRight
+          onClick={nextSlide}
+          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer select-none w-10 h-10"
+        />
+        <div className="absolute flex gap-1 left-1/2 translate-x-[-50%] bottom-0 pb-1 b">
+          {images.map((_, i) => (
+            <img
+              onClick={() => moveSlide(i)}
+              key={i}
+              src={i === current ? "/fill_circle.svg" : "/circle.svg"}
+              alt={`슬라이드 ${i}번 버튼`}
+              className="cursor-pointer select-none"
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
