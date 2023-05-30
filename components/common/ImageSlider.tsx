d07d0fb49684f44316dcbc2e7cb6d4e489c2188f
tnghgks <tnghgks@naver.com>
2023-05-30 23:41:27 +0900

    design: select-none 옵션 추가

--- components/common/ImageSlider.tsx
+++ components/common/ImageSlider.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState, useCallback } from "react";
 import { BsChevronCompactLeft, BsChevronCompactRight } from "react-icons/bs";
 interface IProps {
   images: string[];
@@ -10,28 +10,29 @@ export default function ImageSlider({ images = [] }: IProps) {
   const [current, setCurrent] = useState(0);
   const timerId = useRef<NodeJS.Timer>();
 
-  const prevSlide = () => {
+  const prevSlide = useCallback(() => {
     const isFirst = current === 0;
     const newIndex = isFirst ? images.length - 1 : current - 1;
     setCurrent(newIndex);
-  };
+  }, [current, images]);
 
-  const nextSlide = () => {
+  const nextSlide = useCallback(() => {
     const isLast = current === images.length - 1;
     const newIndex = isLast ? 0 : current + 1;
     setCurrent(newIndex);
-  };
-  const moveSlide = (newIndex) => {
+  }, [current, images]);
+
+  const moveSlide = (newIndex: number) => {
     setCurrent(newIndex);
   };
 
   useEffect(() => {
     timerId.current = setInterval(() => {
-      console.log("실행");
+      nextSlide();
     }, 3000);
 
     return () => clearInterval(timerId.current);
-  }, []);
+  }, [nextSlide]);
 
   return (
     <div className="w-full h-[550px] ">
@@ -39,17 +40,17 @@ export default function ImageSlider({ images = [] }: IProps) {
         <img
           src={images[current]}
           alt={images[current]}
-          className={`w-full h-full object-cover shrink-0 `}
+          className={`w-full h-full object-cover shrink-0 select-none select-none`}
         />
 
         <BsChevronCompactLeft
           onClick={prevSlide}
-          className="hidden  group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+          className="hidden  group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] left-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer select-none"
         />
 
         <BsChevronCompactRight
           onClick={nextSlide}
-          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer"
+          className="hidden group-hover:block absolute top-[50%] -translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer select-none"
         />
 
         <div className="absolute flex gap-1 left-1/2 bottom-0 pb-1">
@@ -59,7 +60,7 @@ export default function ImageSlider({ images = [] }: IProps) {
               key={i}
               src={i === current ? "/fill_circle.svg" : "/circle.svg"}
               alt={`슬라이드 ${i}번 버튼`}
-              className="cursor-pointer"
+              className="cursor-pointer select-none"
             />
           ))}
         </div>
