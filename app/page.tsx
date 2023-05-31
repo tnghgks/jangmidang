e46a2590614d1912586396e0fdeab604ce3e8d00
tnghgks <tnghgks@naver.com>
2023-06-01 00:21:02 +0900

    design: IntroCard Container 반응형 클래스 추가

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
               "https://jangmidang.s3.ap-northeast-2.amazonaws.com/bd1c1f13194d4127b1a808851d5dc1cc_1440.jpg",
             ]}
           />
-          <div className=" w-full h-[301px] gap-5 flex justify-start px-11 mt-9 mb-6">
+          <div className="w-full h-[301px] gap-5 flex justify-start px-11 mt-9 mb-6 flex-wrap md:flex-nowrap">
             <IntroCard bgSrc="https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg">
               아이디어스
             </IntroCard>
