1aa45948f0a78b61ad6c671eeb58149845058406
tnghgks <tnghgks@naver.com>
2023-06-01 23:32:52 +0900

    feat: 클릭 시 링크 주소 추가

--- components/IntroCard.tsx
+++ components/IntroCard.tsx
@@ -1,20 +1,22 @@
 import { PropsWithChildren } from "react";
 
 interface IProps {
+  link?: string;
   bgSrc: string;
 }
 
-export default function IntroCard({ bgSrc, children }: PropsWithChildren<IProps>) {
+export default function IntroCard({ link, bgSrc, children }: PropsWithChildren<IProps>) {
   return (
-    <div
+    <a
       style={{
         backgroundImage: `url(${bgSrc ? bgSrc : ""})`,
       }}
       className={
         "flex w-full rounded-2xl bg-gray-600 text-white items-center justify-center bg-cover bg-no-repeat shadow-[1.95px_1.95px_2.6px_rgba(0,0,0,0.15)]"
       }
+      href={link}
     >
-      <span className="text-2xl font-bo">{children}</span>
-    </div>
+      <span className="text-2xl font-bold">{children}</span>
+    </a>
   );
 }
