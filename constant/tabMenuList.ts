5f02162111aabf2e2a0d675673f4d3ef366caecb
tnghgks <tnghgks@naver.com>
2023-06-03 19:27:21 +0900

    feat: 작업일지 링크 수정

--- constant/tabMenuList.ts
+++ constant/tabMenuList.ts
@@ -2,7 +2,11 @@ export const tabMenuList = [
   { id: 1, title: "단품메뉴", src: "/singleMenu" },
   { id: 2, title: "선물세트", src: "/giftSet" },
   { id: 3, title: "답례품", src: "/returnGift" },
-  { id: 4, title: "작업일지", src: "/workLog" },
+  {
+    id: 4,
+    title: "작업일지",
+    src: "https://www.instagram.com/stories/highlights/17923553386660509/",
+  },
   { id: 5, title: "매장위치", src: "/place" },
   { id: 6, title: "온라인쇼핑몰", src: "https://smartstore.naver.com/jangmidang" },
 ];
