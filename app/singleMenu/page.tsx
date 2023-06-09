07baca161c9c4f002a92454723f0afecaca9cf8b
tnghgks <tnghgks@naver.com>
2023-06-09 23:14:24 +0900

    feat: ProductCard 수정 및 page에 추가

--- app/singleMenu/page.tsx
+++ app/singleMenu/page.tsx
@@ -1,12 +1,46 @@
+import TagList from "@/components/moecules/TagList";
 import FilterHeader from "@/components/organisms/FilterHeader";
+import ProductCardList from "@/components/organisms/ProductCardList";
 
 export default function page() {
   return (
-    <div className="bg-slate-100 p-5">
+    <div className="bg-slate-100 p-5 min-h-screen">
       <FilterHeader />
-      <div className="grid md:grid-cols-4">
-        <div className="flex">d</div>
-      </div>
+      <TagList tagList={["test", "Test"]} />
+      <ProductCardList
+        cards={[
+          {
+            id: 1,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+          {
+            id: 2,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+          {
+            id: 3,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+          {
+            id: 4,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+          {
+            id: 5,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+        ]}
+      />
     </div>
   );
 }
---
+++ types/product.ts
@@ -0,0 +1,6 @@
+export interface IProduct {
+  id: number;
+  img: string;
+  title: string;
+  description: string;
+}
