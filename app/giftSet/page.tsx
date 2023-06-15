423fe6a9de2d6e8c43b9614e412d4f65d4caba44
tnghgks <tnghgks@naver.com>
2023-06-15 19:46:30 +0900

    feat: giftSet 페이지 생성

--- app/giftSet/page.tsx
+++ app/giftSet/page.tsx
@@ -1,3 +1,51 @@
+import TagList from "@/components/moecules/TagList";
+import FilterHeader from "@/components/organisms/FilterHeader";
+import ProductCardList from "@/components/organisms/ProductCardList";
+
+export const metadata = {
+  title: "선물세트 | 장미당",
+  description: "전통디저트 선물세트 소개",
+};
+
 export default function page() {
-  return <div>page</div>;
+  return (
+    <div className="bg-slate-100 p-5 min-h-screen">
+      <FilterHeader />
+      <TagList tagList={["test", "Test"]} />
+      <ProductCardList
+        cards={[
+          {
+            id: 1,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+          {
+            id: 2,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+          {
+            id: 3,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+          {
+            id: 4,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+          {
+            id: 5,
+            img: "https://jangmidang.s3.ap-northeast-2.amazonaws.com/a430ef4f75ff49f7b9d74d71fadb0801.jpg",
+            title: "제목",
+            description: "설명",
+          },
+        ]}
+      />
+    </div>
+  );
 }
