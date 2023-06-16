156968668d87bebc4868a044241b37bf62d86882
tnghgks <tnghgks@naver.com>
2023-06-16 23:39:11 +0900

    feat: 단품 및 선물세트 페이지 DB 연결

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <body className={inter.className}>
         <Header />
-        {children}
+        <div className="bg-slate-100 p-5 min-h-screen">{children}</div>
         <Footer />
       </body>
     </html>
--- components/organisms/ProductCardList.tsx
+++ components/organisms/ProductCardList.tsx
@@ -2,14 +2,14 @@ import { IProduct } from "@/types/product";
 import ProductCard from "../moecules/ProductCard";
 
 interface IProps {
-  cards: IProduct[];
+  products: IProduct[];
 }
 
-export default function ProductCardList({ cards = [] }: IProps) {
+export default function ProductCardList({ products = [] }: IProps) {
   return (
     <div className="flex flex-col py-5 gap-5 sm:grid sm:grid-cols-2 md:grid-cols-4 xl:grid-cols-5">
-      {cards.map((card, i) => (
-        <ProductCard key={i} cardData={card} />
+      {products.map((product) => (
+        <ProductCard key={product._id} productData={product} />
       ))}
     </div>
   );
