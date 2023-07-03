5c4d26338086fbe3403e9636d1a5ac7627354f17
tnghgks <tnghgks@naver.com>
2023-07-03 23:43:18 +0900

    fix: input props 타입 변경으로 인한 수정

--- app/components/molecule/SearchBar.tsx
+++ app/components/molecule/SearchBar.tsx
@@ -10,7 +10,7 @@ export default function SearchBar() {
   return (
     <form>
       <Input
-        size="lg"
+        scale="lg"
         icon={<Icon icon={<FaSearch size={24} />} size="lg" onClick={handleClick} />}
       />
     </form>
