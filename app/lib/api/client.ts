d7e55e11c0e3893bd8929b14a5aea9b564f744c8
tnghgks <tnghgks@naver.com>
2023-07-07 20:43:39 +0900

    feat: product 생성 client api 생성

---
+++ app/lib/api/client.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_URL,
+  timeout: 1000,
+});
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    return config;
+  },
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
+
+axiosInstance.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    console.log(error);
+    return Promise.reject(error);
+  }
+);
