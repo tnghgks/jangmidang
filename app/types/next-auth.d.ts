1a43ab2f8de526efa15be0752081875e19f1b4a9
tnghgks <tnghgks@naver.com>
2023-07-07 20:10:13 +0900

    feat: jwt token 에 role 정보 추가

--- app/types/next-auth.d.ts
+++ app/types/next-auth.d.ts
@@ -1,7 +1,12 @@
 import { JWT } from "next-auth/jwt";
+import NextAuth, { DefaultSession } from "next-auth";
+import { IUser } from "./user";
 
 declare module "next-auth/jwt" {
   interface JWT {
-    user: { name?: string; email?: string };
+    user: { name?: string; email?: string; role?: string };
   }
+}
+declare module "next-auth" {
+  interface User extends Partial<IUser> {}
 }
