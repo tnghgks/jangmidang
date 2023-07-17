857a8be2561ed49fb623ee041271cabdf5cbfb43
tnghgks <tnghgks@naver.com>
2023-07-17 23:54:50 +0900

    fix: axios intercaptor error 수정

--- app/lib/api/client.ts
+++ app/lib/api/client.ts
@@ -18,13 +18,8 @@ axiosInstance.interceptors.request.use(
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response.status === 400) {
-      alert("잘못된 요청입니다.");
-    } else if (error.response.status === 401) {
-      alert("권한이 없습니다.");
-    } else {
-      alert(error);
-    }
+    alert(error);
+
     return Promise.reject(error);
   }
 );
