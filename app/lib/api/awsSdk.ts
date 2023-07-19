3df81a5edd4cb8825ac672530cca80923863bc2d
tnghgks <tnghgks@naver.com>
2023-07-19 22:40:46 +0900

    feat: aws-sdk 관련 함수 이동 및 listObjects 함수 추가

--- app/lib/api/awsSdk.ts
+++ app/lib/api/awsSdk.ts
@@ -29,3 +29,27 @@ export const uploadS3 = (file: File, filename: string) => {
     return alert("업로드에 실패하였습니다.");
   }
 };
+
+export const getListObjectS3 = async (path: string) => {
+  const REGION = process.env.NEXT_PUBLIC_AWS_REGION;
+  const ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
+  const SECRET_ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY_ID;
+
+  AWS.config.update({
+    region: REGION,
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY_ID,
+  });
+
+  const params = {
+    Bucket: "jangmidang",
+    Prefix: path,
+    StartAfter: path,
+  };
+
+  const s3 = new AWS.S3();
+  return await s3
+    .listObjectsV2(params)
+    .promise() // 메소드를 프로미스 객체화
+    .then((data) => data);
+};
