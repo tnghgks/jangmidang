90e570e9d74ae3132220c2e341b7e68c2e86f622
tnghgks <tnghgks@naver.com>
2023-08-01 14:30:02 +0900

    Style: AwsS3 관련 함수들 단일 클래스로 변경

--- app/lib/api/awsSdk.ts
+++ app/lib/api/awsSdk.ts
@@ -1,55 +1,52 @@
 import AWS from "aws-sdk";
 
-export const uploadS3 = (file: File, filename: string) => {
-  const REGION = process.env.NEXT_PUBLIC_AWS_REGION;
-  const ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
-  const SECRET_ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY_ID;
+export default class AwsS3{
+  private Region;
+  private AccessKeyId;
+  private SecretAccessKeyId;
 
-  AWS.config.update({
-    region: REGION,
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_ACCESS_KEY_ID,
-  });
-
-  if (file && filename) {
-    const upload = new AWS.S3.ManagedUpload({
-      params: {
-        ACL: "public-read",
-        Bucket: "jangmidang",
-        Key: `upload/${filename}`,
-        Body: file,
-      },
+  constructor(){
+    this.Region= process.env.NEXT_PUBLIC_AWS_REGION;
+    this.AccessKeyId= process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
+    this.SecretAccessKeyId= process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY_ID;
+    
+    AWS.config.update({
+      region: this.Region,
+      accessKeyId: this.AccessKeyId,
+      secretAccessKey: this.SecretAccessKeyId,
     });
-
-    return upload
-      .promise()
-      .then((result) => result.Location)
-      .catch((err) => console.log(err));
-  } else {
-    return alert("업로드에 실패하였습니다.");
   }
-};
 
-export const getListObjectS3 = async (path: string) => {
-  const REGION = process.env.NEXT_PUBLIC_AWS_REGION;
-  const ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID;
-  const SECRET_ACCESS_KEY_ID = process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY_ID;
-
-  AWS.config.update({
-    region: REGION,
-    accessKeyId: ACCESS_KEY_ID,
-    secretAccessKey: SECRET_ACCESS_KEY_ID,
-  });
-
-  const params = {
-    Bucket: "jangmidang",
-    Prefix: path,
-    StartAfter: path,
-  };
-
-  const s3 = new AWS.S3();
-  return await s3
-    .listObjectsV2(params)
-    .promise() // 메소드를 프로미스 객체화
-    .then((data) => data);
-};
+  upload(file: File, filename: string){
+    if (file && filename) {
+      const upload = new AWS.S3.ManagedUpload({
+        params: {
+          ACL: "public-read",
+          Bucket: "jangmidang",
+          Key: `upload/${filename}`,
+          Body: file,
+        },
+      });
+  
+      return upload
+        .promise()
+        .then((result) => result.Location)
+        .catch((err) => console.log(err));
+    } else {
+      return alert("업로드에 실패하였습니다.");
+    }
+  }
+  async getByPath(path: string){
+    const params = {
+      Bucket: "jangmidang",
+      Prefix: path,
+      StartAfter: path,
+    };
+  
+    const s3 = new AWS.S3();
+    return await s3
+      .listObjectsV2(params)
+      .promise() // 메소드를 프로미스 객체화
+      .then((data) => data);
+  }
+}
